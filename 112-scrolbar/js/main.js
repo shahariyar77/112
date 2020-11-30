e3ec2cdcf12dd34a36d79fcd6bc2d9ac39061ca2
shahariyar Nafis <72545687+shahariyar77@users.noreply.github.com>
2020-11-30 19:58:46 +0600

    Add files via upload

---
+++ 112-scrolbar/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+ $("body").niceScroll({
+        cursorcolor:"blue",
+    	cursorwidth: "10px",
+    	autohidemode: false,
+    	cursoropacitymax: .8,
+ });
+    $(".another-scroolbar").niceScroll({
+ 		cursorcolor:"red",
+ 		autohidemode: false,
+    });
+
+   
+});
